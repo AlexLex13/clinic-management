a64172dc02c3e7aa3856a1f0639b8241065d25dc
Alexey <suxovarov.alexey@gmail.com>
2023-04-20 10:29:56 +0300

    change img size

--- src/scenes/ourServices/Service.tsx
+++ src/scenes/ourServices/Service.tsx
@@ -10,11 +10,11 @@ const Service = ({ name, image }: Props) => {
   opacity-0 transition duration-500 hover:opacity-90`;
 
   return (
-    <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+    <li className="relative mx-5 inline-block h-[200px] w-[450px]">
       <div className={overlayStyles}>
         <p className="text-2xl">{name}</p>
       </div>
-      <img alt={`${image}`} src={image} />
+      <img alt={`${image}`} src={image} height="300px" width="300px"/>
     </li>
   );
 };
